13c962cf34456c8466f8874107e46472c9ccf87d
Pasha Fistanto <pasha@fstn.top>
2026-07-01 12:14:39 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "CXLMemSim Flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = with pkgs;
+      mkShell {
+        packages = [
+          clang-tools
+          bear
+          cmake
+          linuxHeaders
+          spdlog
+          cxxopts
+        ];
+      };
+  };
+}
